398a6f7451fcd9649803d61c213b64ba00ca1e4e
testdemo12271 <tarun.kumar@eccouncil.org>
2022-10-11 01:09:54 -0400

    irt commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using AuthenticationandAuthorization.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace AuthenticationandAuthorization.Controllers
 {
@@ -23,20 +26,83 @@ namespace AuthenticationandAuthorization.Controllers
         {
             return View();
         }
-        [Authorize]
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
         public IActionResult Securing()
         {
             return View();
         }
         [HttpGet("login")]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Validate(string username, string password, string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("login");
+            }
+            ClaimsIdentity identity = null;
+            bool isAuthenticate = false;
+
+            ViewData["ReturnUrl"] = returnUrl;
+            if(username == "bob" || password == "admin@123")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim("username", username));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                var claims_identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claims_Principle = new ClaimsPrincipal(claims_identity);
+                await HttpContext.SignInAsync(claims_Principle);
+                return Redirect(returnUrl);
+            }
+            
+
+            if (username == "alice" && password == "user@123")
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name,username),
+                    new Claim(ClaimTypes.Role,"User")
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                isAuthenticate = true;
+            }
+            if (isAuthenticate)
+            {
+                var principal = new ClaimsPrincipal(identity);
+                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToAction("Index", "Home");
+            }
+            
+            
+            TempData["Wrongcredientials"] = "Wrongcredientials. Wrong username or password";
+            return View("Login");
+        }
+
+            [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Authorize(Roles ="Admin")]
+        public IActionResult UserDetail()
+        {
+            return View();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/denied";
+                    options.Events = new CookieAuthenticationEvents()
+                    {
+                        OnSigningIn = async context =>
+                        {
+                            var principal = context.Principal;
+                           
+                            await Task.CompletedTask;
+                        },
+                        //New Changes
+                        OnSignedIn = async context =>
+                        {
+                            await Task.CompletedTask;
+                        },
+                        OnValidatePrincipal = async context =>
+                        {
+                            await Task.CompletedTask;
+                        }
+                    };
+                });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +43,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
